65f5aa9d895af9c01076046c0c0860b5461760f9
Ken Tu <kentu@Kens-MacBook-Pro.local>
2026-01-08 13:53:43 +0000

    Correctly linked firebase

--- src/unAuth/components/Bumpups.js
+++ src/unAuth/components/Bumpups.js
@@ -8,7 +8,7 @@ const Bumpups = () => {
       <h2>Ken Tu</h2>
       <p>This is a fullstack project built using AI and a YouTube tutorial to allow me, Ken Tu, 
         to explore prompt engineering and compiling a project using a tech stack of GitHub, Firebase, React,
-        JavaScript, CSS etc.. This project also allows me to explore simple sofware industry skills like version 
+        JavaScript, CSS etc.. This project also allows me to explore simple software industry skills like version 
         control, modularization, and research. 
       </p>
     </div>
